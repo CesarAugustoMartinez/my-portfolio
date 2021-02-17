eab69b92c96b236115701ba489632e6a30640fc0
Cesar Martinez <cesaguma@hotmail.com>
2021-02-16 23:28:49 -0500

    Creating Intro component to handle all the information to be used in this section.

---
+++ src/components/Sections/Intro.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Intro() {
+  return (
+      <section id="intro" className="section scrollspy full-height">
+        <div className="overlay"></div>
+        <div className="container">
+          <h2 style={{ marginTop: "65px" }}>
+            I really enjoy <span class="teal">working hard</span>
+            <br />
+            and always
+            <span className="underline">do my best</span>.
+          </h2>
+        </div>
+      </section>
+  );
+}
+
+export default Intro;
