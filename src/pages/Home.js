f33aa53cf309f6bb9f04c4543037d83f18728549
Cesar Martinez <cesaguma@hotmail.com>
2021-02-16 23:33:33 -0500

    Importing Intro, About, Experience, Projects, and Skills components in the Home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,26 @@
 import React, { useState } from "react";
 import Container from "../components/Container/Container";
 import Navbar from "../components/Navbar/Navbar";
+import About from "../components/Sections/About";
+import Experience from "../components/Sections/Experience";
+import Intro from "../components/Sections/Intro";
+import Projects from "../components/Sections/Projects";
+import Skills from "../components/Sections/Skills";
 function Home(props) {
-    const [sidebar, setSidebar] = useState(false);
-    const showSidebar = () => setSidebar(!sidebar);
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
 
   return (
     <>
-        <Navbar sidebar={sidebar} isActive={showSidebar} />
-        <Container sidebar={sidebar} isActive={showSidebar}>
-        <section id="intro" className="section scrollspy full-height">
-            <div className="overlay"></div>
-            <div className="container">
-                <h2>
-                    I really enjoy <span class="teal">working hard</span><br/>and always
-                    <span className="underline">do my best</span>.
-                </h2>
+      <Navbar sidebar={sidebar} isActive={showSidebar} />
+      <Container sidebar={sidebar} isActive={showSidebar}>
+        <Intro/> 
+        <About/>
+        <Experience/>
+        <Projects/>
+        <Skills/>
 
-            </div>
-        </section>
-          
-        </Container>      
+      </Container>
     </>
   );
 }
