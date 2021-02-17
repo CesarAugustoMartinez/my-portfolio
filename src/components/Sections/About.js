b93851949da1636032d68839bd597950704a6d9d
Cesar Martinez <cesaguma@hotmail.com>
2021-02-16 23:28:06 -0500

    Creating About component to handle all the information to be used in this section.

---
+++ src/components/Sections/About.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function About() {
+    return (
+        <section id="about" class="section scrollspy full-height">
+          <h3 className="page-title white-text teal">About</h3>
+          <div className="container flow-text">
+            <div class="card">
+              <div class="card-content">
+                <div class="row"></div>
+                <p style={{ textAlign: "justify", marginTop:"25px"}}>
+                  I am Cesar Augusto Martinez A. I live in Gaithersburg,
+                  Maryland. I am a Full-Stack Web Developer with a Bachelor's
+                  degree in Systems Engineering and a proven database and GIS
+                  background. A graduate of Johns Hopkins University with a
+                  certificate in Full Stack Web Development expanding on
+                  programming languages and technologies like HTML, CSS,
+                  JavaScript, jQuery, Node.js, React, C++, PHP, and databases as Oracle, MySQL,
+                  MongoDB and Access.
+                </p>
+                <p style={{ textAlign: "justify", marginTop:"25px" }}>
+                  Ability to solve complex problems and track record as a
+                  General Manager and Co-owner of Msportswear. Demonstrated
+                  experience managing projects for IT systems and manufacturing
+                  processes using the latest technologies in this area. A
+                  penchant professional with an aptitude to work independently
+                  and collaboratively to drive feasible outcomes under pressure.
+                  Looking to leverage web development and database; to create
+                  intuitive apps to help people and improve systems.
+                </p>
+                <p style={{ textAlign: "justify", marginTop:"25px" }}>
+                  I was a Elite speed skater in my country. I represent
+                  provincial and national team for international competitions.
+                  My Hobby is biking or skating. I love spending time with my
+                  children.
+                </p>               
+              </div>
+            </div>
+          </div>
+        </section>
+    )
+}
+
+export default About
