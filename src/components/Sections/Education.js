abc2b563f57f34599c31915014cf8b97a18fb351
Cesar Martinez <cesaguma@hotmail.com>
2021-02-19 21:32:10 -0500

    Creating the Education.js file to create the Education component to be used into the home page.

---
+++ src/components/Sections/Education.js
@@ -0,0 +1,100 @@
+import React from 'react'
+
+function Education() {
+    return (
+        <section id="education" className="section scrollspy">
+            <h3 className="page-title white-text teal">Education</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col s12 m6">
+                        <div className="card">
+                            <div className="card-content">
+                                <p>
+                                    <span className="card-title"><a href="https://www.jhu.edu/"
+                                            className="teal-text hoverline">Johns Hopkins University</a></span>
+                                </p>
+                                <p className="blue-text">Baltimore, MD</p>
+                                <ul>
+                                    <li>Bootcamp Certificate in Full Stack Web Development</li>
+                                    <p>A 24-week intensive program focused on gaining technical programming skills in
+                                        HTML5, CSS3, JavaScript, jQuery, Bootstrap, Firebase, Node.js, MySQL, MongoDB,
+                                        Express, Handelbars.js, and ReactJS.</p>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col s12 m6">
+                        <div className="card">
+                            <div className="card-content">
+                                <p>
+                                    <span className="card-title"><a href="http://www.gwu.edu"
+                                            className="teal-text hoverline">Montgomery College</a></span>
+                                </p>
+                                <p className="blue-text">Germantown, MD</p>
+                                <ul>
+                                    <li>Cisco CCNA Routing and Switching Program</li>
+                                </ul>
+                                <p>A 12-week intensive program focused on Construct simple LANs, perform basic
+                                    configurations for routers and switches, implement IP addressing schemes, and
+                                    resolve common issues with RIPv1, RIPv2, single-area and multi-area OSPF, virtual
+                                    LANs, and inter-VLAN routing in both IPv4 and IPv6 networks.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col s12 m6">
+                        <div className="card">
+                            <div className="card-content">
+                                <p>
+                                    <span className="card-title"><a href="https://uazuay.edu.ec/"
+                                            className="teal-text hoverline">University of Azuay</a></span>
+                                </p>
+                                <p className="blue-text">Cuenca, Azuay - Ecuador</p>
+                                <ul>
+                                    <li>Bachelor’s Degree - Information Systems Engineering</li>
+                                    <p>US Degree equivalence evaluation from WES available upon request</p>
+                                    <span><h6>Technologies and Systems</h6><br/>
+                                        •	<strong>Databases:</strong>  Oracle, MySQL, Access, SQL Server<br/>
+                                        •	<strong>Developer Programs:</strong>  Oracle JDeveloper 11g, Visual Basic 6.0, ASP.NET (Web Forms, MVC), Visual C++, Prolog, Dreamweaver, Oracle Forms, MySQL Workbench, Data Warehouse, NetLogo 4.1.3<br/>
+                                        •	<strong>Geographic Information Systems:</strong>  Arcgis 9.3, Arcgis 10.0, ArcScene 10.0, ENVI, IDRISI SELVA<br/>
+                                        •	<strong>Programming Languages:</strong>  Java,C++, XML, HTML, PHP, ADF, BASIC, JavaScript<br/> 
+                                        •	<strong>Networking Protocols:</strong>  DNS, DHCP, HTTP, OSPF, EIGRP, TFTP, Frame Relay, ATM2<br/>
+                                        •	<strong>Operating Systems:</strong>  Windows, Linux, MAC OS<br/>
+                                    </span>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>    
+                    <div className="col s12 m6">
+                        <div className="card">
+                            <div className="card-content">
+                                <p>
+                                    <span className="card-title"><a href="https://uazuay.edu.ec/"
+                                            className="teal-text hoverline">University of Azuay</a></span>
+                                </p>
+                                <p className="blue-text">Cuenca, Azuay - Ecuador</p>
+                                <ul>
+                                    <li>Masters Candidate - Geographic Information Systems with emphasis on Land Planning</li>
+                                    <span><h6> Knowledge/Experience</h6><br/>
+                                        •	Spatial reference systems and Global satellite navigation systems<br/>
+                                        •	Aerial Photographs and Photo restoration<br/>  
+                                        •	Geodatabases and network analysis<br/> 
+                                        •	Satellite Teledetection and Teledetection Sensors<br/>
+                                        •	Temporal Analysis of Teledetection Output<br/>
+                                        •	Exploratory Analysis of Spatial Data and Infrastructure for Spatial Data and Map Servers<br/> 
+                                        •	Advanced Analysis of Geographic Data (socioeconomic variables)<br/>
+                                        •	Cartographic Quality Control, Land Surveys, and Valuation<br/> 
+                                        •	Management and Use of Hidrographic Bodies<br/>
+                                        •	Environmental Protection, Land Use Planning, and Assessment and Restructuring<br/>
+                                        </span>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>    
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Education
